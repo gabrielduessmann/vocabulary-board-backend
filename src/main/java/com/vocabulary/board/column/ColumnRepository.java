769a7dc50b5cd09f93fd62571b9af56cd5e050a5
Duessmann <gabriel.duessmann@gmail.com>
2021-07-31 16:05:25 -0300

    fix repository error

--- src/main/java/com/vocabulary/board/column/ColumnRepository.java
+++ src/main/java/com/vocabulary/board/column/ColumnRepository.java
@@ -2,11 +2,7 @@ package com.vocabulary.board.column;
 
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface ColumnRepository extends CrudRepository<Column, UUID> {
-    List<Column> findAllByOOrderBySprintOrderAscSprintOrderIs();
-    List<Column> findAllBySprintOrderIsNotNullAndOrderBySprintOrderAsc();
-    List<Column> findAllByStatus();
 }
